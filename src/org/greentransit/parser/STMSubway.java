4801a5a917c796b7a909bff12b95761a4ec5c9c3
Mathieu Méa <mathieu.mea@gmail.com>
2018-06-14 09:44:05 -0400

    Compatibility with latest update.

--- src/org/greentransit/parser/STMSubway.java
+++ src/org/greentransit/parser/STMSubway.java
@@ -25,7 +25,7 @@ public class STMSubway implements GAgencyTools {
 
 	public static final String ROUTE_ID_FILTER = null;
 	public static final String ROUTE_TYPE_FILTER = "1"; // subway only
-	public static final String SERVICE_ID_FILTER = "18M";
+	public static final String SERVICE_ID_FILTER = "18U";
 	public static final String STOP_ID_FILTER = null;
 	public static final int THREAD_POOL_SIZE = 4;
 
